da73755c1b69e17b50b9a6da0d2176d01a629157
Matheus Alves <mathalves@proton.me>
2025-04-18 12:00:30 -0300

    clap init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Target URL to benchmark
+    #[arg(short, long)]
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("URL: {}", args.url)
 }
