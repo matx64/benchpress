569a10e098a44b47495bb94ea8380b04a014c60c
Matheus Alves <mathalves@proton.me>
2025-04-18 13:46:32 -0300

    add sequential requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use reqwest::{Client, Error};
+use std::time::Duration;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -6,10 +8,45 @@ struct Args {
     /// Target URL to benchmark
     #[arg(short, long)]
     url: String,
+
+    /// Total number of requests
+    #[arg(short, long)]
+    requests: u32,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
-    println!("URL: {}", args.url)
+    println!("Starting benchmark...");
+    println!("URL: {}", args.url);
+    println!("Total Requests: {}", args.requests);
+
+    let client = Client::builder()
+        .timeout(Duration::from_secs(30))
+        .build()
+        .expect("Failed to build Client");
+
+    execute(&client, args).await;
+}
+
+async fn execute(client: &Client, args: Args) {
+    let mut ok_count = 0;
+    let mut err_count = 0;
+
+    for _ in 0..args.requests {
+        match req(&client, &args.url).await {
+            Ok(is_ok) if is_ok => ok_count += 1,
+            _ => err_count += 1,
+        }
+    }
+
+    println!("\n------ Results ------");
+    println!("Successful requests: {}", ok_count);
+    println!("Failed requests: {}", err_count);
+}
+
+async fn req(client: &Client, url: &String) -> Result<bool, Error> {
+    let status = client.get(url).send().await?.status();
+    Ok(!status.is_server_error() && !status.is_client_error())
 }
